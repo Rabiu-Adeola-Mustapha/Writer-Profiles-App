917c7d5def078e007fd2dd0aa48d7af6864c748e
Mustapha <konnect1795@hmail.com>
2022-03-23 14:54:12 +0100

    React Component 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import writers from "./writers"
+// import writers from "./writers"
 import './App.css';
 import {ProfileCard} from "./ProfileCard";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [writers, setWriters] = useState([]);
+
+  useEffect(() => {
+    const getWriters = async () => {
+      const response = await fetch('/writers.json');
+      const data = await response.json();
+      setWriters(data);
+    }
+
+    getWriters()
+  
+}, []);
+
+
+
   return (
     <div >
       <h1>Writer Profiles</h1>
