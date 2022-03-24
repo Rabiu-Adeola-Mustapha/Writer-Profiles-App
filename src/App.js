71795f854ad6d520a912862fd3ea1990ef4e5148
Mustapha <konnect1795@hmail.com>
2022-03-24 14:50:54 +0100

    React Component 4

--- src/App.js
+++ src/App.js
@@ -1,33 +1,64 @@
 // import writers from "./writers"
 import './App.css';
-import {ProfileCard} from "./ProfileCard";
+import ProfileCard from "./ProfileCard";
 import { useEffect, useState } from "react";
 
 
 function App() {
-  const [writers, setWriters] = useState([]);
+  const [data, setData] = useState({
+    writers: [],
+    loading: false,
+  });
 
-  useEffect(() => {
-    const getWriters = async () => {
-      const response = await fetch('/writers.json');
-      const data = await response.json();
-      setWriters(data);
-    }
-
-    getWriters()
+  const handleClick = () => {
+    setData((prevData) => ({
+      ...prevData,
+      loading: true
+    }))
+    
+    setTimeout(() => {
+      const getWriters = async () => {
+        const response = await fetch('/writers.json');
+        const data = await response.json();
+        setData(data);
+      };
   
-}, []);
+      getWriters()
+    
+    }, 2000);
+  };
 
-
-
+if(data.loading) {
   return (
+    <div>
+      <h1> Writer Profiles</h1>
+      <div className='container'>
+        <div className="card action">
+          <p className="infoText"> ...Loading</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+return (
     <div >
       <h1>Writer Profiles</h1>
       <div className="container">
         
-        {writers.map((writer) => (
-          <ProfileCard writer={writer} />
-        )
+        {data.writers.length === 0 ? (
+
+          <div className="card action">
+            <p className='inforText'>Oops... no writer profile found</p>
+            <button className='actionBtn' onClick={handleClick}>
+              Get Writers
+            </button>
+          </div>
+        ) : (
+          data.writers.map((writer) => (
+            <ProfileCard key={writer.id} writer={writer} />
+          ))
+        
               
         )}
       </div>
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import { useState } from "react";
 
-export const ProfileCard = ({writer}) => {
+function ProfileCard({ writer }) {
+  const [showBio, setShowBio] = useState(false);
+  
+  const handleClick = (bioData) => {
+    setShowBio(!showBio);
+  };
+
   return (
-    <div className='card'>
-
-         <img 
-         src= {`/images/${writer.avatar}.png`}
-          width="300px"
-          height="300px"
-          alt="" />
-          <div className="textGroup">
-            <h3>{writer.name}</h3>
-            <p>{writer.email}</p>
-            <p>{writer.phone}</p>
+    <div className="card">
+      <div className="cardContent">
+        {showBio ? (
+          <div className="bioWrap">
+            <p className="bio">{writer.bio}</p>
           </div>
+        ) : (
+          <img
+            src={`images/${writer.avatar}.png`}
+            width="300px"
+            alt={writer.img}
+          />
+        )}
+      </div>
+      <div className="textGroup">
+        <h3>{writer.name}</h3>
+        <p>{writer.email}</p>
+        <p>{writer.phone}</p>
+
+        <button className="actionBtn" onClick={() => handleClick(writer.bio)}>
+          {showBio ? "View Image" : "Read Bio"}
+        </button>
+      </div>
     </div>
-  )
+  );
 }
+
+export default ProfileCard;
