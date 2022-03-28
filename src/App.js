6516e61203afbd410140300333b83b1484b80607
Mustapha <konnect1795@hmail.com>
2022-03-28 15:53:07 +0100

    React componet 5

--- src/App.js
+++ src/App.js
@@ -2,71 +2,70 @@
 import './App.css';
 import ProfileCard from "./ProfileCard";
 import { useEffect, useState } from "react";
+import ProfileForm from './components/ProfileForm';
 
 
 function App() {
-  const [data, setData] = useState({
-    writers: [],
-    loading: false,
-  });
 
-  const handleClick = () => {
-    setData((prevData) => ({
-      ...prevData,
-      loading: true
-    }))
+  const[allProfile, setAllProfile] = useState([
+    {
+      firstName: "Hananah",
+      lastName: "Montana",
+      email: "hannah.montana@email.com",
+      phone:"+233 024 455 000"
+    },
+  ])
+
+  const Submit = (profile) => {
+    const arr = allProfile;
+    arr.push(profile);
+    setAllProfile(arr);
+  }
+//   const [data, setData] = useState({
+//     writers: [],
+//     loading: false,
+//   });
+
+//   const handleClick = () => {
+//     setData((prevData) => ({
+//       ...prevData,
+//       loading: true
+//     }))
     
-    setTimeout(() => {
-      const getWriters = async () => {
-        const response = await fetch('/writers.json');
-        const data = await response.json();
-        setData(data);
-      };
+//     setTimeout(() => {
+//       const getWriters = async () => {
+//         const response = await fetch('/writers.json');
+//         const data = await response.json();
+//         setData(data);
+//       };
   
-      getWriters()
+//       getWriters()
     
-    }, 2000);
-  };
+//     }, 2000);
+//   };
 
-if(data.loading) {
-  return (
-    <div>
-      <h1> Writer Profiles</h1>
-      <div className='container'>
-        <div className="card action">
-          <p className="infoText"> ...Loading</p>
-        </div>
-      </div>
-    </div>
-  );
-}
+// if(data.loading) {
+//   return (
+//     <div>
+//       <h1> Writer Profiles</h1>
+//       <div className='container'>
+//         <div className="card action">
+//           <p className="infoText"> ...Loading</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
 return (
     <div >
       <h1>Writer Profiles</h1>
       <div className="container">
-        
-        {data.writers.length === 0 ? (
-
-          <div className="card action">
-            <p className='inforText'>Oops... no writer profile found</p>
-            <button className='actionBtn' onClick={handleClick}>
-              Get Writers
-            </button>
-          </div>
-        ) : (
-          data.writers.map((writer) => (
-            <ProfileCard key={writer.id} writer={writer} />
-          ))
-        
-              
-        )}
+          <ProfileForm Submit={Submit}/>
+          {allProfile.map((writer) => (
+            <ProfileCard key={writer.id} writer={writer} />        
+        ))}
       </div>
-
-
-
-
-
     </div>
   );
 }
---
+++ src/components/ProfileForm.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+const ProfileForm = ({Submit}) => {
+    const [profile, setProfile] = useState({
+        firstName:"",
+        lastName: "",
+        email: "",
+        phone:"",
+    });
+
+    const handleChange =(e) => {
+        const {name, value} = e.target;
+        setProfile({...profile, [name]:value});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        Submit(profile);
+
+    }
+
+
+  return (
+    <from type="submit" OnSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Personal Information</legend>
+        <input
+          type="text"
+          name="firstName"
+          onChange={handleChange}
+          value={profile.firstName}
+        />
+
+        <input
+          type="text"
+          name="lastName"
+          onChange={handleChange}
+          value={profile.lastName}
+        />
+
+        <input
+          type="text"
+          name="email"
+          onChange={handleChange}
+          value={profile.email}
+        />
+
+        <input
+          type="tel"
+          name="phone"
+          onChange={handleChange}
+          value={profile.phone}
+        />
+      </fieldset>
+    </from>
+  );
+        
+  
+}
+
+export default ProfileForm
